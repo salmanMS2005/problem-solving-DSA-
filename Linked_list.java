c9c5b635dec3b0cb476620eb5b3bce4c7f2e3bd4
salmanMS2005 <160301930+salmanMS2005@users.noreply.github.com>
2025-06-18 16:48:06 +0530

    2nd question in Linked_list.java

--- Linked_list.java
+++ Linked_list.java
@@ -1,4 +1,4 @@
-/*leet code :83. Remove Duplicates from Sorted List
+/*LEETCODE :83. Remove Duplicates from Sorted List
 Given the head of a sorted linked list, delete all duplicates such that each element appears only once. Return the linked list sorted as well.*/
 
 // SOLUTION:
@@ -24,5 +24,40 @@ class Solution {
         }
       }
       return head;
+    }
+}
+
+
+/* LEETCODE : 21. Merge Two Sorted Lists
+
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+
+Return the head of the merged linked list. */
+
+//SOLUTION :
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1==null) return list2;
+        if(list2==null) return list1;
+        ListNode dummyhead = new ListNode();
+        ListNode current=dummyhead;
+        
+        while(list1!=null && list2!=null){
+            if(list1.val < list2.val){
+                current.next=list1;
+                list1=list1.next;
+                current=current.next;
+            }else{
+                current.next=list2;
+                list2=list2.next;
+                current=current.next;
+            }
+        }
+        if(list1!=null) current.next=list1;
+        if(list2!=null) current.next=list2;
+        return dummyhead.next;
     }
 }
