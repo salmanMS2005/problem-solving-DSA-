b652ef6584a9391343f2f40b7332244febed5693
salmanMS2005 <160301930+salmanMS2005@users.noreply.github.com>
2025-06-20 16:11:49 +0530

    Update LEETCODE_Linkedlist.java

--- Linked_list/LEETCODE_Linkedlist.java
+++ Linked_list/LEETCODE_Linkedlist.java
@@ -281,3 +281,74 @@ class Solution206 {
         return prev;
     }
 }
+
+
+/*
+
+
+92. Reverse Linked List II
+
+Given the head of a singly linked list and two integers left and right where left <= right, reverse the nodes of the list from position left to position right, and return the reversed list.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], left = 2, right = 4
+Output: [1,4,3,2,5]
+*/
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution92 {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if(left==right){
+            return head;
+        }
+        ListNode prev=null;
+        ListNode current=head;
+
+        
+        for(int i=0;current!=null && i<left-1;i++){
+            prev=current;
+            current=current.next;
+        }
+        ListNode last = prev;
+        ListNode newEnd=current;
+
+        ListNode next=current.next;
+        for(int i=0;current !=null && i<right-left+1;i++){
+            current.next=prev;
+            prev=current;
+            current=next;
+            if(next!=null){
+                next=next.next;
+            }
+        }
+
+        if(last!=null){
+            last.next=prev;
+        }else{
+            head=prev;
+        }
+
+        newEnd.next=current;
+        return head;
+        }
+    }
+
+
+
+
+
