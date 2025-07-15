fc1413fde16f699d44f33a482f9b817d7c767c6d
salmanMS2005 <160301930+salmanMS2005@users.noreply.github.com>
2025-07-15 22:20:35 +0530

    Update subsets.java

--- dsa_approaches/subsets.java
+++ dsa_approaches/subsets.java
@@ -4,12 +4,16 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr={1,2,3};
+        int[] arr1={1,2,3};
+        int[] arr2={1,2,2};
         System.out.println(Arrays.toString(arr));
-        List<List<Integer>> ans=subsets(arr);
-        for(List<Integer> num : ans){
+        List<List<Integer>> ans1=subsets(arr1);
+        for(List<Integer> num : ans1){
             System.out.println(num);
         }
+        List<List<Integer>> ans2=subsetduplicates(arr2);
+        for(List<Integer> num : ans2){
+            System.out.println(num);
     }
 
 static List<List<Integer>> subsets (int[] arr){
@@ -35,4 +39,36 @@ static List<List<Integer>> subsets (int[] arr){
 [1, 3]
 [2, 3]
 [1, 2, 3]
+*/
+
+
+static List<List<Integer>> subsetduplicates(int[] arr){
+    Arrays.sort(arr);
+    List<List<Integer>> outer =new ArrayList<>();
+    outer.add(new ArrayList<>());
+    int start=0;
+    int end=0;
+    for(int i=0;i<arr.length;i++){
+        if(i>0 && arr[i]==arr[i-1]){
+            start=end+1;
+        }
+        end=outer.size()-1;
+        int n=outer.size();
+        for(int j=start;j<n;j++){
+            ArrayList<Integer> inner=new ArrayList<>(outer.get(j));
+            inner.add(arr[i]);
+            outer.add(inner);
+        }
+    }
+    return outer;
+}
+
+/*
+output:
+[]
+[1]
+[2]
+[1, 2]
+[2, 2]
+[1, 2, 2]
 */
