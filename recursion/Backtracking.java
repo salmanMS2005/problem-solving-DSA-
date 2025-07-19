bff37e1dce6c734623f16c422850d9792e89d488
salmanMS2005 <160301930+salmanMS2005@users.noreply.github.com>
2025-07-19 20:35:10 +0530

    Update Backtracking.java

--- recursion/Backtracking.java
+++ recursion/Backtracking.java
@@ -24,6 +24,20 @@ public class Maze {
         int nK=4;
         boolean[][] boardK=new boolean[nK][nK];
         System.out.println(NKnights(boardK,0,0,4));
+        //sudoku
+        char[][] board = {
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
+        };
+        solveSudoku(board);
+        printBoard(board);
 
   }
 //=====================================================================================================================================
@@ -313,4 +327,84 @@ K___
 
 */
 //==================================================================================================================================
+    public static void solveSudoku(char[][] board) {
+        helper(board);
+    }
+    public static boolean helper(char[][] board){
+        int n=board.length;
+        int r=-1;
+        int c=-1;
+        boolean emptyleft=true;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(board[i][j]=='.'){
+                    r=i;
+                    c=j;
+                    emptyleft=false;
+                    break;
+                }
+            }
+             if(emptyleft==false) {
+                break;
+            }
+        }
+        if(emptyleft==true) {
+            return true;
+        }
+        for(int k=1;k<=9;k++){
+            if(isSafe(board,r,c,(char)(k+'0'))){
+                board[r][c]=(char)(k+'0');
+                if(helper(board)){
+                    return true;
+                }else{
+                    board[r][c]='.';
+                }
+            }
+        }
+        return false;
+    }
+    public  static boolean isSafe(char[][] board,int row,int col,char num){
+        for(int i=0;i<board.length;i++){
+                if(board[row][i]==num){
+                    return false;
+                }   
+        }
+        for(int i=0;i<board.length;i++){
+            if(board[i][col]==num){
+                return false;
+            }
+        }
+        int len=(int)(Math.sqrt(board.length));
+        int strow=row-row%len;
+        int stcol=col-col%len;
+        for(int i=strow;i<strow+len;i++){
+            for(int j=stcol;j<stcol+len;j++){
+                if(board[i][j]==num){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    public static void printBoard(char[][] board) {
+        for (char[] row : board) {
+            for (char cell : row) {
+                System.out.print(cell + " ");
+            }
+            System.out.println();
+        }
+    }
+/*
+5 3 4 6 7 8 9 1 2 
+6 7 2 1 9 5 3 4 8 
+1 9 8 3 4 2 5 6 7 
+8 5 9 7 6 1 4 2 3 
+4 2 6 8 5 3 7 9 1 
+7 1 3 9 2 4 8 5 6 
+9 6 1 5 3 7 2 8 4 
+2 8 7 4 1 9 6 3 5 
+3 4 5 2 8 6 1 7 9 
+*/
+//===============================================================================================================================
+
 }
