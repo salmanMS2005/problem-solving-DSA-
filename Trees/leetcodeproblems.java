5aafca2d4fde54f8da73a368084b0d77400d5b72
salmanMS2005 <160301930+salmanMS2005@users.noreply.github.com>
2025-07-23 00:49:03 +0530

    Update leetcodeproblems.java

--- Trees/leetcodeproblems.java
+++ Trees/leetcodeproblems.java
@@ -252,3 +252,99 @@ class Solution {
     }
 }
 //==========================================================================================================================================
+
+/*
+116. Populating Next Right Pointers in Each Node
+You are given a perfect binary tree where all leaves are on the same level, 
+and every parent has two children. The binary tree has the following definition:
+struct Node {
+  int val;
+  Node *left;
+  Node *right;
+  Node *next;
+}
+Populate each next pointer to point to its next right node. If there is no next right node, the next pointer should be set to NULL.
+Initially, all next pointers are set to NULL.
+Example 1:
+Input: root = [1,2,3,4,5,6,7]
+Output: [1,#,2,3,#,4,5,6,7,#]
+Explanation: Given the above perfect binary tree (Figure A), your function should populate each next pointer to point to its next right node, just like in Figure B. The serialized output is in level order as connected by the next pointers, with '#' signifying the end of each level.
+Example 2:
+Input: root = []
+Output: []
+
+//Answer:
+2 approaches:
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+//o(n) space complexity:
+/*class Solution {
+    public Node connect(Node root) {
+        if(root==null){
+            return null;
+        }
+        Queue<Node> que=new LinkedList<>();
+        que.offer(root);
+        while(!que.isEmpty()){
+            int size=que.size();
+            Node prevnode=null;
+            for(int i=0;i<size;i++){
+                Node node=que.poll();
+                if(prevnode!=null){
+                    prevnode.next=node;
+                }
+                prevnode=node;
+                if(node.left!=null){
+                    que.offer(node.left);
+                }
+                if(node.right!=null){
+                    que.offer(node.right);
+                }
+            }
+        }
+        return root;
+    }
+}*/
+
+//o(1) space complexity:
+    class Solution {
+    public Node connect(Node root){
+        if(root==null){
+            return null;
+        }
+
+        Node leftmost=root;
+
+        while(leftmost.left!=null){
+            Node current=leftmost;
+            while(current!=null){
+                current.left.next=current.right;
+                if(current.next!=null){
+                    current.right.next=current.next.left;
+                }
+                current=current.next;
+            }
+            leftmost=leftmost.left;
+        }
+        return root;
+    }
+}
