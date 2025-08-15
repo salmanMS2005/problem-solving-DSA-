6b9a3898df82e0e350c4f96993b03005f4cdd089
salmanMS2005 <160301930+salmanMS2005@users.noreply.github.com>
2025-08-16 01:17:28 +0530

    Update leetcode.java

--- recursion/leetcode.java
+++ recursion/leetcode.java
@@ -410,3 +410,151 @@ class Solution {
     }
 }
 //==============================================================================================================
+/*
+Subset Sums
+Difficulty: MediumAccuracy: 72.55%Submissions: 171K+Points: 4
+Given a array arr of integers, return the sums of all subsets in the list.  Return the sums in any order.
+
+Examples:
+
+Input: arr[] = [2, 3]
+Output: [0, 2, 3, 5]
+Explanation: When no elements are taken then Sum = 0. When only 2 is taken then Sum = 2. When only 3 is taken then Sum = 3. When elements 2 and 3 are taken then Sum = 2+3 = 5.
+Input: arr[] = [1, 2, 1]
+Output: [0, 1, 1, 2, 2, 3, 3, 4]
+Explanation: The possible subset sums are 0 (no elements), 1 (either of the 1's), 2 (the element 2), and their combinations.
+Input: arr[] = [5, 6, 7]
+Output: [0, 5, 6, 7, 11, 12, 13, 18]
+Explanation: The possible subset sums are 0 (no elements), 5, 6, 7, and their combinations.
+Constraints:
+1 ≤ arr.size() ≤ 15
+0 ≤ arr[i] ≤ 104
+*/
+// User function Template for Java//User function Template for Java
+class Solution {
+    public ArrayList<Integer> subsetSums(int[] arr) {
+        // code here
+        ArrayList<Integer> ans=new ArrayList<>();
+        helper(0,arr,ans,0);
+        return ans;
+    }
+    static void helper(int ind,int[] arr,ArrayList<Integer> ans,int count){
+         if(ind==arr.length){
+             ans.add(count);
+             return;
+         }
+         count+=arr[ind];
+         helper(ind+1,arr,ans,count);
+         count-=arr[ind];
+         helper(ind+1,arr,ans,count);
+    }
+}
+//=======================================================================================================
+/*
+90. Subsets II
+
+Given an integer array nums that may contain duplicates, return all possible subsets (the power set).
+
+The solution set must not contain duplicate subsets. Return the solution in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,2]
+Output: [[],[1],[1,2],[1,2,2],[2],[2,2]]
+Example 2:
+
+Input: nums = [0]
+Output: [[],[0]]
+ 
+
+Constraints:
+
+1 <= nums.length <= 10
+-10 <= nums[i] <= 10
+*/
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> ans=new ArrayList<>();
+        helper(0,nums,ans,new ArrayList<>());
+        return ans;
+    }
+    static void helper(int ind,int[] nums,List<List<Integer>> ans,List<Integer> ds){
+
+        ans.add(new ArrayList<>(ds));
+
+        for(int i=ind;i<nums.length;i++){
+            if(i!=ind && nums[i]==nums[i-1]){
+                continue;
+            }
+            ds.add(nums[i]);
+            helper(i+1,nums,ans,ds);
+            ds.remove(ds.size()-1);
+        }
+    }
+}
+//================================================================================================
+/*
+216. Combination Sum III
+
+Find all valid combinations of k numbers that sum up to n such that the following conditions are true:
+
+Only numbers 1 through 9 are used.
+Each number is used at most once.
+Return a list of all possible valid combinations. The list must not contain the same combination twice, and the combinations may be returned in any order.
+
+ 
+
+Example 1:
+
+Input: k = 3, n = 7
+Output: [[1,2,4]]
+Explanation:
+1 + 2 + 4 = 7
+There are no other valid combinations.
+Example 2:
+
+Input: k = 3, n = 9
+Output: [[1,2,6],[1,3,5],[2,3,4]]
+Explanation:
+1 + 2 + 6 = 9
+1 + 3 + 5 = 9
+2 + 3 + 4 = 9
+There are no other valid combinations.
+Example 3:
+
+Input: k = 4, n = 1
+Output: []
+Explanation: There are no valid combinations.
+Using 4 different numbers in the range [1,9], the smallest sum we can get is 1+2+3+4 = 10 and since 10 > 1, there are no valid combination.
+ 
+
+Constraints:
+
+2 <= k <= 9
+1 <= n <= 60
+*/
+class Solution {
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        if(n< k*(k+1)/2) return new ArrayList<>();
+        List<List<Integer>> ans=new ArrayList<>();
+        helper(1,k,n,ans,new ArrayList<>());
+        return ans;
+    }
+    static void helper(int nums,int k,int n,List<List<Integer>> ans,List<Integer> ds){
+        if(n==0 && k==0){
+            ans.add(new ArrayList(ds));
+            return;
+        }
+        for(int i=nums;i<=9;i++){
+            if(i>n || k<=0) break;
+                
+            ds.add(i);
+            helper(i+1,k-1,n-i,ans,ds);
+            ds.remove(ds.size()-1);
+        }
+    }
+}
+//================================================================================================================
