05a1871e5bc04242808866c7f563fdf45100f11d
salmanMS2005 <160301930+salmanMS2005@users.noreply.github.com>
2025-08-16 01:18:38 +0530

    Update leetcode.java

--- Graphs/dfs_bfs/leetcode.java
+++ Graphs/dfs_bfs/leetcode.java
@@ -1015,5 +1015,85 @@ class Solution {
 }
 //==============================================================================================
 /*
+127. Word Ladder
+Solved
+Hard
+Topics
+premium lock icon
+Companies
+A transformation sequence from word beginWord to word endWord using a dictionary wordList is a sequence of words beginWord -> s1 -> s2 -> ... -> sk such that:
 
+Every adjacent pair of words differs by a single letter.
+Every si for 1 <= i <= k is in wordList. Note that beginWord does not need to be in wordList.
+sk == endWord
+Given two words, beginWord and endWord, and a dictionary wordList, return the number of words in the shortest transformation sequence from beginWord to endWord, or 0 if no such sequence exists.
+
+ 
+
+Example 1:
+
+Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log","cog"]
+Output: 5
+Explanation: One shortest transformation sequence is "hit" -> "hot" -> "dot" -> "dog" -> cog", which is 5 words long.
+Example 2:
+
+Input: beginWord = "hit", endWord = "cog", wordList = ["hot","dot","dog","lot","log"]
+Output: 0
+Explanation: The endWord "cog" is not in wordList, therefore there is no valid transformation sequence.
+ 
+
+Constraints:
+
+1 <= beginWord.length <= 10
+endWord.length == beginWord.length
+1 <= wordList.length <= 5000
+wordList[i].length == beginWord.length
+beginWord, endWord, and wordList[i] consist of lowercase English letters.
+beginWord != endWord
+All the words in wordList are unique.
 */
+class Pair{
+    String first;//word
+    int second;//level
+    public Pair(String first,int second){
+        this.first=first;
+        this.second=second;
+    }
+}
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Queue<Pair> que=new LinkedList<>();
+        Set<String> set=new HashSet<>();
+        que.add(new Pair(beginWord,1));
+
+        //adding wordList into the set to make the count not to repeat
+        for(String word:wordList){
+            set.add(word);
+        }
+        set.remove(beginWord);//removing the first word to not to repeat the counting
+        //bfs
+        while(!que.isEmpty()){
+            Pair pair=que.poll();
+            String word=pair.first;
+            int level=pair.second;
+            //if we found the endWord ,at that time we return our level as the answer
+            if(word.equals(endWord)) return level;
+            //now changing each word's letter to check whether any combination is in the set
+            for(int i=0;i<word.length();i++)
+                for(char ch='a';ch<='z';ch++){
+                    char[] charArrayword=word.toCharArray();
+                    charArrayword[i]=ch;
+                    String newword=new String(charArrayword);
+                    //if the new word is in the given set...then do bfs with the word again by adding it into the queue
+                    if(set.contains(newword)){
+                        que.add(new Pair(newword,level+1));
+                        set.remove(newword);
+                    }
+                }
+        }
+        //if we havent found the endWord in our set...then return 0 as the answer
+        return 0;
+
+    }
+}
+//============================================================================================
