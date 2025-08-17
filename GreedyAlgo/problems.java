b476e001c07eda7be696b6bfc33c3a7bc2825ce3
salmanMS2005 <160301930+salmanMS2005@users.noreply.github.com>
2025-08-18 01:18:58 +0530

    Update problems.java

--- GreedyAlgo/problems.java
+++ GreedyAlgo/problems.java
@@ -182,3 +182,107 @@ class Solution {
     }
 }
 //======================================================================================================================================================
+/*
+
+Code
+Test Result
+Testcase
+Testcase
+55. Jump Game
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+You are given an integer array nums. You are initially positioned at the array's first index, and each element in the array represents your maximum jump length at that position.
+
+Return true if you can reach the last index, or false otherwise.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: true
+Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+
+Input: nums = [3,2,1,0,4]
+Output: false
+Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible to reach the last index.
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+0 <= nums[i] <= 105
+*/
+class Solution {
+    public boolean canJump(int[] nums) {
+        int cover=0;
+        for(int i=0;i<nums.length;i++){
+            if(i>cover) return false;
+            cover=Math.max(cover,i+nums[i]);
+        }
+        return true;
+    }
+}
+//======================================================================================================================================================
+/*
+45. Jump Game II
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+You are given a 0-indexed array of integers nums of length n. You are initially positioned at index 0.
+
+Each element nums[i] represents the maximum length of a forward jump from index i. In other words, if you are at index i, you can jump to any index (i + j) where:
+
+0 <= j <= nums[i] and
+i + j < n
+Return the minimum number of jumps to reach index n - 1. The test cases are generated such that you can reach index n - 1.
+
+ 
+
+Example 1:
+
+Input: nums = [2,3,1,1,4]
+Output: 2
+Explanation: The minimum number of jumps to reach the last index is 2. Jump 1 step from index 0 to 1, then 3 steps to the last index.
+Example 2:
+
+Input: nums = [2,3,0,1,4]
+Output: 2
+ 
+
+Constraints:
+
+1 <= nums.length <= 104
+0 <= nums[i] <= 1000
+It's guaranteed that you can reach nums[n - 1].
+*/
+class Solution {
+    public int jump(int[] nums) {
+     int last=nums.length-1,jump=0,coverage=0,lstjumpidx=0;
+     if(nums.length==1)
+     return 0;
+        for (int i = 0;i<=nums.length-1;i++)
+        {
+            coverage=Math.max(coverage,i+nums[i]);
+            if(i==lstjumpidx)
+            {
+                lstjumpidx=coverage;
+                jump++;
+            
+        
+                if(coverage>=last){
+                    return jump;
+                }
+
+            }
+        }
+        return jump;
+    }
+}
+//======================================================================================================================================================
