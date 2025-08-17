8cde3b8cc6d85ab218efd55e0d415937f1b94242
salmanMS2005 <160301930+salmanMS2005@users.noreply.github.com>
2025-08-18 01:12:43 +0530

    Update leetcode.java

--- Graphs/dfs_bfs/leetcode.java
+++ Graphs/dfs_bfs/leetcode.java
@@ -1097,3 +1097,79 @@ class Solution {
     }
 }
 //============================================================================================
+/*
+79. Word Search
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+
+The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+ 
+
+Example 1:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+Output: true
+Example 2:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+Output: true
+Example 3:
+
+
+Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+Output: false
+ 
+
+Constraints:
+
+m == board.length
+n = board[i].length
+1 <= m, n <= 6
+1 <= word.length <= 15
+board and word consists of only lowercase and uppercase English letters.
+ 
+*/
+class Solution {
+    public boolean exist(char[][] board, String word) {
+        int n=board.length;
+        int m=board[0].length;
+        boolean[][] visited=new boolean[n][m];
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]==word.charAt(0)){
+                    if(dfs(0,word,board,visited,i,j,n,m)){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+    boolean dfs(int ind,String word,char[][] board,boolean[][] visited,int r,int c,int n,int m){
+        if(ind==word.length()){
+            return true;
+        }
+        if(r<0 || c<0|| r>n-1 || c>m-1 || board[r][c]!=word.charAt(ind) || visited[r][c]==true){
+            return false;
+        }
+        visited[r][c]=true;
+
+        boolean up=dfs(ind+1,word,board,visited,r+1,c,n,m);
+        boolean down=dfs(ind+1,word,board,visited,r-1,c,n,m);
+        boolean left=dfs(ind+1,word,board,visited,r,c-1,n,m);
+        boolean right=dfs(ind+1,word,board,visited,r,c+1,n,m);
+
+        visited[r][c]=false;//backtracking
+
+        return (up||down||left||right);
+    }
+}
+//=============================================================================================
