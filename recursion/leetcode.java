b1187a86833718a5cbfbede3107acc6b41f0a0a1
salmanMS2005 <160301930+salmanMS2005@users.noreply.github.com>
2025-08-18 01:11:41 +0530

    Update leetcode.java

--- recursion/leetcode.java
+++ recursion/leetcode.java
@@ -558,3 +558,58 @@ class Solution {
     }
 }
 //================================================================================================================
+/*
+131. Palindrome Partitioning
+Solved
+Medium
+Topics
+premium lock icon
+Companies
+Given a string s, partition s such that every substring of the partition is a palindrome. Return all possible palindrome partitioning of s.
+
+ 
+
+Example 1:
+
+Input: s = "aab"
+Output: [["a","a","b"],["aa","b"]]
+Example 2:
+
+Input: s = "a"
+Output: [["a"]]
+ 
+
+Constraints:
+
+1 <= s.length <= 16
+s contains only lowercase English letters.
+*/
+class Solution {
+    public List<List<String>> partition(String s) {
+       List<List<String>> ans=new ArrayList<>() ;
+       helper(0,s,ans,new ArrayList<>());
+       return ans;
+    }
+    static void helper(int ind,String s,List<List<String>> ans,ArrayList<String> ds){
+        if(ind==s.length()){
+            ans.add(new ArrayList<>(ds));
+            return;
+        }
+        for(int i=ind;i<s.length();i++){
+            if(isPalindrome(s,ind,i)){
+                ds.add(s.substring(ind,i+1));
+                helper(i+1,s,ans,ds);
+                ds.remove(ds.size()-1);
+            }
+        }
+    }
+    static boolean isPalindrome(String s,int start,int end){
+        while(start<=end){
+            if(s.charAt(start++)!=s.charAt(end--)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
+//================================================================================
